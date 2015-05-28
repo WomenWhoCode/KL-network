6392ddb2444177169f31bef6938fc871386c03e1
Lydia Lim <5litez@gmail.com>
2015-05-28 19:42:41 +0800

    Checking in missing file

---
+++ Project-Miki/app/src/main/java/wwckl/projectmiki/PreferenceKeys.java
@@ -0,0 +1,10 @@
+package wwckl.projectmiki;
+
+/**
+ * Created by lydialim on 5/28/15.
+ */
+public final class PreferenceKeys {
+
+    public static final String DISPLAY_WELCOME        = "pref_display_welcome";
+    public static final String DEFAULT_IMAGE_SELECTOR = "pref_input_method";
+}
